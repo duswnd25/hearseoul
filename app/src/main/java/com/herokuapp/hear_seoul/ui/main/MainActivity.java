9c2acb229f7b101e657c8a4b63f12f1e2b22e03b
duswnd25 <duswnd25@gmail.com>
2018-09-10 02:03:57 +0900

    .

--- app/src/main/java/com/herokuapp/hear_seoul/ui/main/MainActivity.java
+++ app/src/main/java/com/herokuapp/hear_seoul/ui/main/MainActivity.java
@@ -67,10 +67,7 @@ public class MainActivity extends AppCompatActivity implements LocationByIP.call
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            startActivity(new Intent(this, SettingsActivity.class));
-            return true;
-        }
+
         return super.onOptionsItemSelected(item);
     }
 
