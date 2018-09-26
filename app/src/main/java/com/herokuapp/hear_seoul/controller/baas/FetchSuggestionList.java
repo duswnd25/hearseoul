c1847d25ed805019f570a2b70bb362acdd77798a
duswnd25 <duswnd25@gmail.com>
2018-09-27 01:10:25 +0900

    추천 목록 가져오기

---
+++ app/src/main/java/com/herokuapp/hear_seoul/controller/baas/FetchSuggestionList.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2018. YeonJung Kim
+ *
+ *  GitHub : @duswnd25
+ *  Site   : https://yeonjung.herokuapp.com/
+ */
+
+package com.herokuapp.hear_seoul.controller.baas;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+
+import com.herokuapp.hear_seoul.R;
+import com.herokuapp.hear_seoul.core.Const;
+import com.herokuapp.hear_seoul.core.Logger;
+import com.skt.baas.api.BaasObject;
+import com.skt.baas.api.BaasQuery;
+import com.skt.baas.callback.BaasListCallback;
+import com.skt.baas.exception.BaasException;
+
+import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
+
+public class FetchSuggestionList {
+    private OnFetchSuggestionListCallback callback;
+    private ProgressDialog loadingProgress;
+
+    public FetchSuggestionList(Context context, OnFetchSuggestionListCallback callback) {
+        this.callback = callback;
+        loadingProgress = new ProgressDialog(context);
+        loadingProgress.setMessage(context.getString(R.string.loading));
+        loadingProgress.setCancelable(false);
+    }
+
+
+    public void getData(LinkedList<String> params) {
+        loadingProgress.show();
+        BaasQuery<BaasObject> baasQuery = BaasQuery.makeQuery(Const.BAAS.SUGGESTION.TABLE_NAME);
+        baasQuery.whereContainedIn(Const.BAAS.SUGGESTION.ID, params);
+        baasQuery.findInBackground(new BaasListCallback<BaasObject>() {
+            @Override
+            public void onSuccess(List<BaasObject> fetchResult, BaasException e) {
+                if (e == null) {
+                    LinkedList<String> result = new LinkedList<>();
+                    Logger.d(String.valueOf(fetchResult.size()));
+                    for (int index = 0; index < fetchResult.size(); index++) {
+                        result.add(fetchResult.get(0).getString(Const.BAAS.SUGGESTION.SUGGEST));
+                    }
+                    loadingProgress.hide();
+                    callback.onDataFetchSuccess(result);
+                } else {
+                    Logger.e(e.getMessage());
+                }
+            }
+        });
+    }
+
+    public interface OnFetchSuggestionListCallback extends Serializable {
+        void onDataFetchSuccess(LinkedList<String> result);
+    }
+}
