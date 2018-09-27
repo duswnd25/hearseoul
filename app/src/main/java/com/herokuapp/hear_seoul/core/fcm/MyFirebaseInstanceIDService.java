d9cf9fb4c6a3e0e7ac78c0cb4ae61c04321837c3
duswnd25 <duswnd25@gmail.com>
2018-09-28 02:55:41 +0900

    fcm

---
+++ app/src/main/java/com/herokuapp/hear_seoul/core/fcm/MyFirebaseInstanceIDService.java
@@ -0,0 +1,14 @@
+package com.herokuapp.hear_seoul.core.fcm;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+
+    @Override
+    public void onTokenRefresh() {
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+    }
+}
