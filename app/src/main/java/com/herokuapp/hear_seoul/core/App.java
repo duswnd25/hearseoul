a3279c1306298b40c90c6382474d53e4a930d84c
duswnd25 <duswnd25@gmail.com>
2018-09-29 01:59:58 +0900

    create notification channel

--- app/src/main/java/com/herokuapp/hear_seoul/core/App.java
+++ app/src/main/java/com/herokuapp/hear_seoul/core/App.java
@@ -8,10 +8,15 @@
 package com.herokuapp.hear_seoul.core;
 
 import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
 
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.herokuapp.hear_seoul.R;
 import com.skt.baas.api.Baas;
+
+import java.util.Objects;
 
 public class App extends Application {
 
@@ -21,5 +26,15 @@ public class App extends Application {
         Baas.init(this, 1, getString(R.string.baas_key));
         Baas.debugMode(false);
         FirebaseMessaging.getInstance().subscribeToTopic("default");
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            String channelId = "default_channel";
+            String channelName = getString(R.string.notification_channel_default_title);
+
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = new NotificationChannel(channelId, channelName, importance);
+            Objects.requireNonNull(notificationManager).createNotificationChannel(mChannel);
+        }
     }
 }
